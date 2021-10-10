25bb1c2a8960e211313889418a664bbd22921e54
Nikhil Chatterjee <nikhilc1527@gmail.com>
2021-10-09 22:23:39 -0700

    made a cabal project

--- Main.hs
+++ Main.hs
@@ -1,20 +1,34 @@
+#!/bin/sh ghc
+
 module Main where
+
 import System.IO
 import System.Environment
+import Data.Trie
 
-myReadFile :: Handle -> IO String
-myReadFile h = do
-  eof <- hIsEOF h
-  if eof
-  then do
-    return ""
-  else do
-    line <- hGetLine h
-    nextLines <- myReadFile h
-    return $ line ++ "\n" ++ nextLines
+-- START_TOKENS
+data Token =
+  SYSCALL           | -- for syscalls, use : and then name of syscall
+  FUNC              | -- a user defined function name
+  VAL               | -- a user defined variable name
+  PTR               | -- a user defined pointer to heap space
+  LITERALINT        | -- literal (4 bytes)
+  LITERALSTRING     | -- literal string with ""
+  IF                | -- if statement
+  THEN              |
+  ELSE              |
+  CURLYBRACKETOPEN  | -- for defining value from a statement
+  CURLYBRACKETCLOSE |
+  PARENOPEN         | -- for function calls
+  PARENCLOSE
+-- END_TOKENS
+
+numTokens :: IO Int
+numTokens = do
+  progName <- getProgName
+  tokens <- readFile (progName ++ ".hs") >>= (return . drop 2 . takeWhile (\ x -> x /= "-- END_TOKENS") . dropWhile (\ x -> x /= "-- START_TOKENS") . lines)
+  return $ length tokens
 
 main :: IO()
 main = do
-  fileInput <- getArgs >>= (return . head)
-  contents <- withFile fileInput ReadMode myReadFile
-  putStrLn contents
+  numTokens >>= print
